c5abe289341a0a14ef9837b81e413d3e983c98fc
Chinmay Patil <chinmaytlc@gmail.com>
2024-01-06 15:57:01 -0700

    added ngfor

--- autoflag/src/app/app.component.ts
+++ autoflag/src/app/app.component.ts
@@ -9,4 +9,23 @@ import {MatButtonModule} from "@angular/material/button";
 export class AppComponent {
   title = 'autoflag';
   showFiller = false;
+
+  tweetdb = [
+    {
+      user: "Shiba Inu",
+      id: "Small",
+      text: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan." +
+        "A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for" +
+        "hunting.",
+      picture: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+    },
+    {
+      user: "Cat",
+      id: "1203123",
+      text: "The cat is dead",
+      picture: "https://material.angular.io/assets/img/examples/shiba2.jpg"
+    },
+  ]
+
+
 }
--- autoflag/src/app/app.module.ts
+++ autoflag/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from "@angular/material/sidenav";
@@ -15,10 +15,11 @@ import {MatCardModule} from "@angular/material/card";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatSidenavModule,
     MatButtonModule,
     MatButtonToggleModule,
-    MatCardModule
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
