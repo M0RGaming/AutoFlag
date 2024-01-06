9e6d2f07eba7e8a21a3d07fd909ae52114360bcf
Nikhil <nnayyar1@ualberta.ca>
2024-01-06 16:57:38 -0700

    backend ready to receive requests

--- flaggerRoutes.py
+++ flaggerRoutes.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 # Load environment variables
 load_dotenv()
 
